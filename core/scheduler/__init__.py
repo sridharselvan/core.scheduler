00e0cc3c7ac3e6e8008919bf4b96f2b1972263f3
Siva Cn <nsivakumar@zeomega.com>
2018-02-25 01:27:24 +0530

    initial commit

---
+++ core/scheduler/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+    Module :mod:``
+
+    This Module is created to...
+
+    LICENSE: The End User license agreement is located at the entry level.
+
+"""
+
+# ----------- START: Native Imports ---------- #
+__import__('pkg_resources').declare_namespace(__name__)
+# ----------- END: Native Imports ---------- #
+
+# ----------- START: Third Party Imports ---------- #
+# ----------- END: Third Party Imports ---------- #
+
+# ----------- START: In-App Imports ---------- #
+# ----------- END: In-App Imports ---------- #
+
+
+__all__ = [
+    # All public symbols go here.
+]
