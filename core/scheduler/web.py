039c1048f98b346cd5e3c9daa8c5ed45f653aa19
sridhar S <sridharselvan@protonmail.com>
2018-02-25 13:21:38 +0530

    removed unwanted imports

--- core/scheduler/web.py
+++ core/scheduler/web.py
@@ -11,53 +11,6 @@
 """
 
 # ----------- START: Native Imports ---------- #
-import os
-import re
-import json
-from datetime import datetime
-#import simplejson as json
-
-from ConfigParser import SafeConfigParser
-# ----------- END: Native Imports ---------- #
-
-# ----------- START: Third Party Imports ---------- #
-from bottle import request
-# ----------- END: Third Party Imports ---------- #
-
-# ----------- START: In-App Imports ---------- #
-from core.backend.constants import (
-    RULES_CONFIG_FILE,
-    MASTER_CONFIG_FILE,
-    CLIENT_CONFIG_FILE,
-    NODE_TEMPLATE_CONFIG_FILE,
-    CLIENT_TEMPLATE_CONFIG_FILE
-)
-
-from core.backend.utils.core_utils import (
-    get_unique_id, AutoSession, get_loggedin_user_id
-)
-
-from core.db.model import (
-    CodeScheduleTypeModel, JobDetailsModel
-)
-# ----------- END: In-App Imports ---------- #
-
-
-__all__ = [
-    # All public symbols go here.
-]# -*- coding: utf-8 -*-
-
-"""
-
-    Module :mod:``
-
-    This Module is created to...
-
-    LICENSE: The End User license agreement is located at the entry level.
-
-"""
-
-# ----------- START: Native Imports ---------- #
 import json
 from datetime import datetime
 # ----------- END: Native Imports ---------- #
